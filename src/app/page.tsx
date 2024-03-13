2dd1a3b7617903d8b77803f306ca1724870f0bc7
Kunga Tashi <kuntashtashi11@gmail.com>
2024-03-13 15:54:53 +0530

    Text editor UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { TextEditor } from "@/components/editor";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen ">
-
+    <main className="min-h-screen flex justify-center bg-slate-100 p-4">
+      <TextEditor />
     </main>
   );
 }
---
+++ src/components/editor.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
+
+export const TextEditor = () => {
+  const [value, setValue] = useState("<p>Write something here</p>");
+  return (
+    <div className="max-w-[800px] w-full flex flex-col gap-y-4 items-center">
+      {/* Container for avatars  */}
+      <div className="bg-white w-full rounded-lg shadow-md p-2 h-10">
+        
+      </div>
+      {/* Editor */}
+      <div className="flex-1 bg-white w-full rounded-lg shadow-md overflow-hidden p-4">
+        <ReactQuill
+          value={value}
+          onChange={(changedHTML) => {
+            setValue(changedHTML);
+          }}
+          theme="bubble"
+          className="h-full"
+        />
+      </div>
+    </div>
+  );
+};
